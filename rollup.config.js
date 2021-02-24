8a534e68b5f71d5946703b65dd06d1a91b8b94f0
Carl Hörberg <carl@84codes.com>
2021-02-24 23:38:10 +0100

    rollup two versions, one ESM and one for compability

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,26 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-  input: 'src/amqp-websocket-client.mjs',
-  output: [{
-    file: 'dist/amqp-websocket-client.mjs',
+export default [
+  {
+    input: 'src/amqp-websocket-client.mjs',
+    output: [{
+      file: 'dist/amqp-websocket-client.mjs',
+    }],
+    plugins: [json()]
   }, {
-    file: 'dist/amqp-websocket-client.js',
-    exports: 'default',
-    format: 'cjs'
-  }],
-  plugins: [
-    json(),
-    resolve({ browser: true }),
-    babel({ babelHelpers: 'bundled' }),
-    commonjs(),
-  ]
-};
+    input: 'src/amqp-websocket-client.mjs',
+    output: {
+      file: 'dist/amqp-websocket-client.js',
+      exports: 'default',
+      format: 'iife',
+      name: 'AMQPWebsocketClient',
+    },
+    plugins: [
+      json(),
+      resolve({ browser: true }),
+      babel({ babelHelpers: 'bundled' }),
+      commonjs(),
+    ]
+  }
+];
