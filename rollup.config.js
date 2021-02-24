1b7cdc0b0e395d0d72c311e7c783d5e12ffa5dcc
Carl Hörberg <carl@84codes.com>
2021-02-24 23:05:01 +0100

    Add babel and core-js to add polyfills to dist version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,21 @@
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default {
   input: 'src/amqp-websocket-client.mjs',
-  output: {
-    file: 'dist/amqp-websocket-client.mjs'
-  }
+  output: [{
+    file: 'dist/amqp-websocket-client.mjs',
+  }, {
+    file: 'dist/amqp-websocket-client.js',
+    exports: 'default',
+    format: 'cjs'
+  }],
+  plugins: [
+    json(),
+    resolve({ browser: true }),
+    babel({ babelHelpers: 'bundled' }),
+    commonjs(),
+  ]
 };
