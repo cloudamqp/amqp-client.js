e19d6bfead52e17fd1fcb658d7e8ba19353f0b2e
Carl Hörberg <carl@84codes.com>
2021-02-24 23:08:15 +0100

    Use rollup json plugin to include version in bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+import json from '@rollup/plugin-json'
+
 export default {
   input: 'src/amqp-websocket-client.mjs',
   output: {
     file: 'dist/amqp-websocket-client.mjs'
-  }
+  },
+  plugins: [ json() ]
 };
