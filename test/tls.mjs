09e0092ccce307d1c7aa6595815333a0121b1ea6
Carl Hörberg <carl@84codes.com>
2021-12-17 10:39:34 +0100

    use env AMQPS_URL in tls test

--- test/tls.mjs
+++ test/tls.mjs
@@ -1,8 +1,9 @@
 import test from 'ava';
+import process from 'process';
 import AMQPClient from '../src/amqp-socket-client.mjs';
 
 test('can connect with TLS', t => {
-  const amqp = new AMQPClient(process.env.AMQP_URL)
+  const amqp = new AMQPClient(process.env.AMQPS_URL || "amqps://localhost")
   return amqp.connect()
     .then(conn => conn.channel())
     .then(ch => t.is(ch.connection.channels.length, 2)) // 2 because channel 0 is counted
