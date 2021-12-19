94eaab612ccf008e96ae7330f5b2594c22f45dec
Carl Hörberg <carl@84codes.com>
2021-12-20 00:43:40 +0100

    revert to stable typescript

--- rollup.config.js
+++ rollup.config.js
@@ -6,8 +6,8 @@ export default [
     input: 'src/amqp-websocket-client.ts',
     plugins: [typescript({target: "es6"}), cleanup()],
     output: {
-      sourcemap: 'dist/amqp-websocket-client.mjs.map',
-      file: 'dist/amqp-websocket-client.mjs'
+      file: 'dist/amqp-websocket-client.mjs',
+      sourcemap: 'dist/amqp-websocket-client.mjs.map'
     }
   }
 ]
