344342229c265927dbdbf42d9396ded7882d4d53
Carl Hörberg <carl@84codes.com>
2021-12-20 09:16:00 +0100

    correct UA for node/browser in websocket client

--- src/amqp-websocket-client.ts
+++ src/amqp-websocket-client.ts
@@ -65,9 +65,9 @@ export default class AMQPWebSocketClient extends AMQPBaseClient {
   }
 
   static platform(): string {
-   if (global.window)
-     return global.window.navigator.userAgent
-   else
-     return `${process.release.name} ${process.version} ${process.platform} ${process.arch}`
+    if (typeof(window) !== 'undefined')
+      return window.navigator.userAgent
+    else
+      return `${process.release.name} ${process.version} ${process.platform} ${process.arch}`
   }
 }
