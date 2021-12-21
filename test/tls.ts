f15f5a214b908e52ad0af0b83d98d7321543d1e8
Anton Dalgren <anton@84codes.com>
2021-12-21 11:52:30 +0100

    Specs run on 127.0.0.1 instead of localhost.
    Problem with localhost pointing towards the IPv6 version instead of IPv4. Forcing the IP to 127.0.0.1 solves that.

--- test/tls.ts
+++ test/tls.ts
@@ -2,7 +2,7 @@ import test from 'ava';
 import { AMQPClient } from '../src/index.js';
 
 test('can connect with TLS', t => {
-  const amqp = new AMQPClient(process.env["AMQPS_URL"] || "amqps://localhost?insecure=true")
+  const amqp = new AMQPClient(process.env["AMQPS_URL"] || "amqps://127.0.0.1?insecure=true")
   return amqp.connect()
     .then(conn => conn.channel())
     .then(ch => t.is(ch.connection.channels.length, 2)) // 2 because channel 0 is counted
