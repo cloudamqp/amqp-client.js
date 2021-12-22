68e983b18e8bf0659c2d2ba1bc715812552163a6
Carl Hörberg <carl@84codes.com>
2021-12-22 20:30:23 +0100

    distribute both esm and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,15 @@ import typescript from '@rollup/plugin-typescript'
 export default [
   {
     input: 'src/amqp-websocket-client.ts',
-    plugins: [typescript({target: "es6", lib: ["es6", "dom"], removeComments: true})],
+    plugins: [typescript({
+      target: "es6",
+      module: "es2020",
+      lib: ["es6", "dom"],
+      outDir: "dist",
+      declarationDir: "dist",
+      removeComments: true,
+      exclude: ["src/amqp-client.ts"]
+    })],
     output: {
       file: 'dist/amqp-websocket-client.mjs',
       sourcemap: 'dist/amqp-websocket-client.mjs.map'
