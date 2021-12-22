eecbe18e18cbc633f5cbc39ec918a48930ec8973
Carl Hörberg <carl@84codes.com>
2021-12-22 22:06:14 +0100

    smarter imports in tests

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,6 @@
 import test from 'ava';
-import { AMQPClient, AMQPMessage } from '../src/index.js';
+import AMQPClient from '../src/amqp-socket-client.js';
+import AMQPMessage from '../src/amqp-message.js';
 
 test('can parse the url correctly', t => {
   const username = 'user_name'
