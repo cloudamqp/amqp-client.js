f4e73e895fd9681b57b741f0133eeaf49c617606
Carl Hörberg <carl@84codes.com>
2021-12-22 02:02:20 +0100

    remove rollup cleanup (not needed anymore)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
-import cleanup from 'rollup-plugin-cleanup'
 import typescript from '@rollup/plugin-typescript'
 
 export default [
   {
     input: 'src/amqp-websocket-client.ts',
-    plugins: [typescript({target: "es6"}), cleanup()],
+    plugins: [typescript({target: "es6", lib: ["es6", "dom"], removeComments: true})],
     output: {
       file: 'dist/amqp-websocket-client.mjs',
       sourcemap: 'dist/amqp-websocket-client.mjs.map'
