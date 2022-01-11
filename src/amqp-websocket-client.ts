c86e3e7b143e4446ed6c0eef11b7a98fc33a5e7d
Carl Hörberg <carl@84codes.com>
2022-01-11 22:56:11 +0100

    support split frames over websockets
    basically coping the code from the socket client

--- src/amqp-websocket-client.ts
+++ src/amqp-websocket-client.ts
@@ -23,7 +23,7 @@ export default class AMQPWebSocketClient extends AMQPBaseClient {
     const socket = new WebSocket(this.url)
     this.socket = socket
     socket.binaryType = "arraybuffer"
-    socket.onmessage = (event) => this.parseFrames(new AMQPView(event.data))
+    socket.onmessage = this.handleMessage
     return new Promise((resolve, reject) => {
       this.connectPromise = [resolve, reject]
       socket.onclose = reject
@@ -53,6 +53,61 @@ export default class AMQPWebSocketClient extends AMQPBaseClient {
 
   protected override closeSocket() {
     if (this.socket) this.socket.close()
+  }
+
+  private framePos = 0
+  private frameSize = 0
+  private frameBuffer = new Uint8Array(4096)
+
+  private handleMessage(event: MessageEvent) {
+    const buf : ArrayBuffer = event.data
+    const bufView = new DataView(buf)
+    // A socket read can contain 0 or more frames, so find frame boundries
+    let bufPos = 0
+    while (bufPos < buf.byteLength) {
+      // read frame size of next frame
+      if (this.frameSize === 0) {
+        // first 7 bytes of a frame was split over two reads, this reads the second part
+        if (this.framePos !== 0) {
+          const len = buf.byteLength - bufPos
+          this.frameBuffer.set(new Uint8Array(buf, bufPos), this.framePos)
+          this.frameSize = new DataView(this.frameBuffer).getInt32(bufPos + 3) + 8
+          this.framePos += len
+          bufPos += len
+          continue
+        }
+        // frame header is split over multiple reads, copy to frameBuffer
+        if (bufPos + 3 + 4 > buf.byteLength) {
+          const len = buf.byteLength - bufPos
+          this.frameBuffer.set(new Uint8Array(buf, bufPos), this.framePos)
+          this.framePos += len
+          break
+        }
+
+        this.frameSize = bufView.getInt32(bufPos + 3) + 8
+
+        // avoid copying if the whole frame is in the read buffer
+        if (buf.byteLength - bufPos >= this.frameSize) {
+          const view = new AMQPView(buf, bufPos, this.frameSize)
+          this.parseFrames(view)
+          bufPos += this.frameSize
+          this.frameSize = 0
+          continue
+        }
+      }
+
+      const leftOfFrame = this.frameSize - this.framePos
+      const copyBytes = Math.min(leftOfFrame, buf.byteLength - bufPos)
+
+      this.frameBuffer.set(new Uint8Array(buf, bufPos, copyBytes), this.framePos)
+      this.framePos += copyBytes
+      bufPos += copyBytes
+      if (this.framePos === this.frameSize) {
+        const view = new AMQPView(this.frameBuffer.buffer, 0, this.frameSize)
+        this.parseFrames(view)
+        this.frameSize = this.framePos = 0
+      }
+    }
   }
 
   static platform(): string {
