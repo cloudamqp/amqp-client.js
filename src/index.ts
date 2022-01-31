12481a29030080a10307ce49aa9fcef36f46f0a2
Carl Hörberg <carl@84codes.com>
2022-01-31 21:40:17 +0100

    export all classes

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import AMQPClient from './amqp-socket-client.js'
-import AMQPWebSocketClient from './amqp-websocket-client.js'
-import AMQPChannel from './amqp-channel.js'
-import AMQPQueue from './amqp-queue.js'
-import AMQPConsumer from './amqp-consumer.js'
-import AMQPError from './amqp-error.js'
-import AMQPMessage from './amqp-message.js'
-export { AMQPProperties, Field } from './amqp-properties.js'
-export { AMQPWebSocketClient, AMQPClient, AMQPChannel, AMQPConsumer, AMQPQueue, AMQPError, AMQPMessage }
