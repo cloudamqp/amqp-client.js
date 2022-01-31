a3b46064c226f431840d1bd27fdafce93b79215b
Carl Hörberg <carl@84codes.com>
2022-02-01 00:34:53 +0100

    Don't rollup cjs/mjs files only websocket for browser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript'
 
 const options = {
-  exclude: ["src/amqp-client.ts"], // includes a ts hack for default export in commonjs module
   removeComments: true, // comments only required in declarations
   declaration: false, // let tsc generate declarations
   declarationMap: false,
@@ -14,23 +13,6 @@ export default [
     output: {
       file: 'dist/amqp-websocket-client.mjs',
       sourcemap: 'dist/amqp-websocket-client.mjs.map'
-    }
-  }, {
-    input: 'src/amqp-socket-client.ts',
-    external: ['buffer', 'net', 'tls'],
-    plugins: [typescript(options)],
-    output: {
-      file: 'dist/amqp-client.cjs',
-      format: 'cjs',
-      sourcemap: 'dist/amqp-client.cjs.map'
-    }
-  }, {
-    input: 'src/amqp-socket-client.ts',
-    external: ['buffer', 'net', 'tls'],
-    plugins: [typescript(options)],
-    output: {
-      file: 'dist/amqp-client.mjs',
-      sourcemap: 'dist/amqp-client.mjs.map'
     }
   }
 ]
