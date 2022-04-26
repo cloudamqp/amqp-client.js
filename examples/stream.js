548bcd39119a63c252e6a2b30a790109704b0c1b
Carl Hörberg <carl@84codes.com>
2022-04-26 14:03:22 +0200

    Stream queue example

---
+++ examples/stream.js
@@ -0,0 +1,15 @@
+import { AMQPClient } from '../lib/mjs/amqp-client.js'
+
+async function run() {
+  const amqp = new AMQPClient("amqp://localhost")
+  const conn = await amqp.connect()
+  let ch = await conn.channel()
+  await ch.prefetch(100)
+  let q = await ch.queue("stream1", {}, {"x-queue-type": "stream" })
+  const consumer = await q.subscribe({noAck: false, args: {"x-stream-offset": "first"}}, (msg) => {
+    console.log(msg.bodyString())
+    msg.ack()
+  })
+  await consumer.wait()
+}
+await run()
