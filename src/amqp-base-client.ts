2368119ee13a68b04f70cf61328c4710ed54f76e
Carl Hörberg <carl@84codes.com>
2022-04-28 14:30:50 +0200

    Pass an AMQPError to onerror instead of a string

--- src/amqp-base-client.ts
+++ src/amqp-base-client.ts
@@ -23,7 +23,7 @@ export abstract class AMQPBaseClient {
   channelMax = 0
   frameMax: number
   heartbeat: number
-  onerror: (reason: string) => void
+  onerror: (error: AMQPError) => void
 
   /**
    * @param name - name of the connection, set in client properties
@@ -41,7 +41,7 @@ export abstract class AMQPBaseClient {
     if (platform) this.platform = platform
     this.channels = [new AMQPChannel(this, 0)]
     this.closed = false
-    this.onerror = (reason: string) => console.error("amqp-client connection closed", reason)
+    this.onerror = (error: AMQPError) => console.error("amqp-client connection closed", error.message)
     if (frameMax < 4096) throw new Error("frameMax must be 4096 or larger")
     this.frameMax = frameMax
     if (heartbeat < 0) throw new Error("heartbeat must be positive")
