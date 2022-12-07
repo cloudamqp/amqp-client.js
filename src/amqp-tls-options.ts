d73f159e85cd4134f3e4088320eb86ef0320b7e9
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2022-12-07 09:51:53 +0100

    change comment accordingly

--- src/amqp-tls-options.ts
+++ src/amqp-tls-options.ts
@@ -3,7 +3,7 @@ import { TlsOptions } from 'tls'
 /** Additional TLS options, for more info check https://nodejs.org/api/tls.html#tlscreatesecurecontextoptions 
  *  @cert Cert chains in PEM format. One cert chain should be provided per private key.
  *  @key Private keys in PEM format. PEM allows the option of private keys being encrypted.
- *        Encrypted keys will be decrypted with options.passphrase.
+ *        Encrypted keys will be decrypted with AMQPTlsOptions.passphrase.
  *  @pfx PFX or PKCS12 encoded private key and certificate chain.
  *       pfx is an alternative to providing key and cert individually.
  *       PFX is usually encrypted, if it is, passphrase will be used to decrypt it.
