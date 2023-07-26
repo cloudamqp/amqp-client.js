3bd8bf3ca466536cdf5ce6f3438d6bd00b36f8a1
Carl Hörberg <carl@84codes.com>
2023-07-26 05:13:55 +0200

    enable tcp keepalive if heartbeats are disabled

--- src/amqp-socket-client.ts
+++ src/amqp-socket-client.ts
@@ -53,12 +53,15 @@ export class AMQPClient extends AMQPBaseClient {
       writable: true,
       enumerable: false // hide it from console.log etc.
     })
+    // enable socket read timeout during connection establishment
     socket.setTimeout((this.heartbeat || 60) * 1000)
+    // enable TCP keepalive if AMQP heartbeats are disabled
+    if (this.heartbeat === 0) socket.setKeepAlive(true, 60)
     return new Promise((resolve, reject) => {
       socket.on('timeout', () => reject(new AMQPError("timeout", this)))
       socket.on('error', (err) => reject(new AMQPError(err.message, this)))
       const onConnect = (conn : AMQPBaseClient) => {
-        socket.setTimeout(this.heartbeat * 1000)
+        socket.setTimeout(this.heartbeat * 1000) // reset timeout if heartbeats are disabled
         resolve(conn)
       }
       this.connectPromise = [onConnect, reject]
